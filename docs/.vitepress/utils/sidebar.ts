3bf526a6c02ea6d2dfe2156f63f81352a1b80b39
ics01-24 <1958272541@qq.com>
2024-09-13 14:47:52 +0800

    change sidebar

--- docs/.vitepress/utils/sidebar.ts
+++ docs/.vitepress/utils/sidebar.ts
@@ -16,11 +16,7 @@ export const sidebar = {
       text: '作业',
       items: [
         { text: '作业一', link: '/homework/hw1' },
-        { text: '作业二', link: '/homework/hw2' },
-        { text: '作业三', link: '/homework/hw3' },
-        { text: '作业四', link: '/homework/hw4' },
-        { text: '作业五', link: '/homework/hw5' },
-        { text: '作业六', link: '/homework/hw6' },
+
       ],
     },
   ],
@@ -29,13 +25,7 @@ export const sidebar = {
       text: '实验',
       items: [
         { text: '实验一', link: '/labs/lab1' },
-        { text: '实验二', link: '/labs/lab2' },
-        { text: '实验三', link: '/labs/lab3' },
-        { text: '实验四', link: '/labs/lab4' },
-        { text: '实验五', link: '/labs/lab5' },
-        { text: '实验六', link: '/labs/lab6' },
-        { text: '实验七', link: '/labs/lab7' },
-        { text: '实验八', link: '/labs/lab8' },
+
       ],
     },
   ],
