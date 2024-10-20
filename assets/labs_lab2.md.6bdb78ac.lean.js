b7a58b319733bddfbe0793df9949e54958b6967d
voyage-li <77057108+voyage-li@users.noreply.github.com>
2024-10-20 13:56:46 +0000

    Update gh-pages to output generated at 399ee0b

---
+++ assets/labs_lab2.md.6bdb78ac.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,o as e,c as t,a as l}from"./app.b1da852f.js";const m=JSON.parse('{"title":"\u5B9E\u9A8C\u4E8C","description":"","frontmatter":{},"headers":[],"relativePath":"labs/lab2.md","lastUpdated":1728294221000}'),r={name:"labs/lab2.md"},s=l("",2),_=[s];function i(o,n,d,c,p,h){return e(),t("div",null,_)}const u=a(r,[["render",i]]);export{m as __pageData,u as default};
